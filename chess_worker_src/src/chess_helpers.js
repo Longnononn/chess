1d637556ec9215b2701e749557cff4d86b13f144
Longnononn <long2010hacker@gmail.com>
2025-09-21 10:50:15 +0700

    Delete chess_worker_src directory

--- chess_worker_src/src/chess_helpers.js
+++
@@ -1,7 +0,0 @@
-import { Chess } from 'chess.js'
-
-export function validateMove(fen, move) {
-  const game = new Chess(fen)
-  const res = game.move(move)
-  return { ok: !!res, fen: game.fen(), move: res }
-}
